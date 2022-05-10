8b7cdc5b7d104cbfaff6fd65323037d4830fa9ae
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-05-10 00:28:11 -0300

    Passing values to Resume Item

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ResumeItem from '../ResumeItem';
 import * as C from './styles';
+import {
+  FaDollarSign,
+  FaRegArrowAltCircleDown,
+  FaRegArrowAltCircleUp,
+} from 'react-icons/fa';
 
 export default function Resume() {
   return (
     <C.Container>
-      <ResumeItem />
-      <ResumeItem />
-      <ResumeItem />
+      <ResumeItem title="Entradas" Icon={FaRegArrowAltCircleUp} value="1000" />
+      <ResumeItem title="Saídas" Icon={FaRegArrowAltCircleDown} value="1000" />
+      <ResumeItem title="Total" Icon={FaDollarSign} value="1000" />
     </C.Container>
   );
 }
